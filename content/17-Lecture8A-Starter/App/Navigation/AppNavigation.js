2c25b2488afba3f6707c35d7a36c2a5868171379
Christine Abernathy <ca.abernathy@fb.com>
2020-05-18 11:58:43 -0500

    [WIP] 8A update to Navigation 5.x

--- content/17-Lecture8A-Starter/App/Navigation/AppNavigation.js
+++ content/17-Lecture8A-Starter/App/Navigation/AppNavigation.js
@@ -1,6 +1,6 @@
-import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react';
 import { Images, Colors, Metrics } from '../Themes'
 import { Entypo } from '@expo/vector-icons';
@@ -9,59 +9,80 @@ import BookmarkScreen from '../Screens/BookmarkScreen'
 import BookmarkViewerScreen from '../Screens/BookmarkViewerScreen'
 import UserProfileScreen from '../Screens/UserProfileScreen'
 
-const HomeStack = createStackNavigator({
-  Home: {screen: HomeScreen},
-  UserProfile: {screen: UserProfileScreen},
-},
-{
-  headerMode: 'float',
-  initialRouteName: 'Home'
-})
+const HomeStack = createStackNavigator();
+const HomeStackNavigator = () => {
+  return (
+    <HomeStack.Navigator
+      initialRouteName='Home'
+      headerMode='float'
+    >
+      <HomeStack.Screen
+        name='Home'
+        component={HomeScreen} />
+      <HomeStack.Screen
+        name='UserProfile'
+        component={UserProfileScreen} />
+    </HomeStack.Navigator>
+  );
+}
 
-const BookmarkStack = createStackNavigator({
-  Bookmarks: {screen: BookmarkScreen},
-  BookmarkViewer: {screen: BookmarkViewerScreen},
-},
-{
-  headerMode: 'float',
-  initialRouteName: 'Bookmarks'
-})
+const BookmarkStack = createStackNavigator();
+const BookmarkStackNavigator = () => {
+  return (
+    <BookmarkStack.Navigator
+      initialRouteName='Bookmarks'
+      headerMode='float'
+    >
+      <BookmarkStack.Screen
+        name='Bookmarks'
+        component={BookmarkScreen} />
+      <BookmarkStack.Screen
+        name='BookmarkViewer'
+        component={BookmarkViewerScreen} />
+    </BookmarkStack.Navigator>
+  );
+}
 
-BookmarkStack.navigationOptions = ({ navigation }) => {
-  return {
-    tabBarLabel: 'Bookmarks',
-    tabBarIcon: ({ tintColor }) => (
-      <Entypo name="bookmark"
-        size={Metrics.icons.medium}
-        color={tintColor} />
-    ),
-  };
-};
+const TabNav = createBottomTabNavigator();
 
-HomeStack.navigationOptions = ({ navigation }) => {
-  return {
-    tabBarLabel: 'Home',
-    tabBarIcon: ({ tintColor }) => (
-      <Entypo name="home"
-        size={Metrics.icons.medium}
-        color={tintColor} />
-    ),
-  };
-};
-
-// Manifest of possible screens
-const TabNav = createBottomTabNavigator({
-  FeedScreen: { screen: HomeStack },
-  BookmarkScreen:   { screen: BookmarkStack },
-}, {
-  // Default config for all screens
-  initialRouteName: 'FeedScreen',
-  tabBarOptions: {
-    activeTintColor: Colors.black,
-    showLabel: true,
-  },
-})
-
-const AppContainer = createAppContainer(TabNav);
+function AppContainer() {
+  return (
+    <NavigationContainer>
+      <TabNav.Navigator
+        initialRouteName='FeedScreen'
+        tabBarOptions={{
+          activeTintColor: Colors.black,
+          showLabel: true,
+        }}
+      >
+        <TabNav.Screen
+          name='FeedScreen'
+          component={HomeStackNavigator}
+          options={{
+            title: 'Bookmarks',
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ tintColor }) => (
+              <Entypo
+                name="home"
+               size={Metrics.icons.medium}
+               color={tintColor} />
+            ),
+          }} />
+        <TabNav.Screen
+          name='BookmarkScreen'
+          component={BookmarkStackNavigator}
+          options={{
+            tabBarLabel: 'Bookmarks',
+            tabBarIcon: ({ tintColor }) => (
+              <Entypo
+                name="bookmark"
+                size={Metrics.icons.medium}
+                color={tintColor} />
+              ),
+          }} />
+      </TabNav.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default AppContainer
--- content/17-Lecture8A-Starter/App/Screens/LoginScreen.js
+++ content/17-Lecture8A-Starter/App/Screens/LoginScreen.js
@@ -1,41 +1,34 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { StyleSheet, Text, SafeAreaView, View, TextInput, Button } from 'react-native';
 import { material } from 'react-native-typography';
 import firestore from '../../firebase';
 import firebase from 'firebase';
 
-export default class LoginScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    const params = navigation.state.params || {};
+export default function LoginScreen({ navigation }) {
+  [signUpName, setSignUpName] = useState('');
+  [signUpEmail, setSignUpEmail] = useState('');
+  [signUpPassword, setSignUpPassword] = useState('');
+  [loginEmail, setLoginEmail] = useState('');
+  [loginPassword, setLoginPassword] = useState('');
+  [errorMessageLogin, setErrorMessageLogin] = useState('');
 
-    return { 
+  useLayoutEffect(() => {
+    navigation.setOptions({
       headerTitle: (
         <View style={{justifyContent: 'center', alignItems: 'center'}}>
           <Text style={material.body2}>Unsplash</Text>
           <Text style={[material.caption, {fontSize: 10}]}>Login</Text>
         </View>
       )
-    };
-  };
+    });
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      signUpName: '',
-      signUpEmail: '',
-      signUpPassword: '',
-      loginEmail: '',
-      loginPassword: '',
-      errorMessageLogin: '',
-    }
-  }
+  }, [navigation])
 
   // Check out this link to learn more about firebase.auth()
   // https://firebase.google.com/docs/reference/node/firebase.auth.Auth
-  signUp = async () => {
+  const signUp = async () => {
     try {
-      const response = await firebase.auth().createUserWithEmailAndPassword(this.state.signUpEmail, this.state.signUpPassword);
+      const response = await firebase.auth().createUserWithEmailAndPassword(signUpEmail, signUpPassword);
       if(response.user) {
         const user = firebase.auth().currentUser;
         var userDocRef = firestore.doc('users/' + user.uid);
@@ -43,79 +36,77 @@ export default class LoginScreen extends React.Component {
         // Since my document doesn't exist, userDocRef.set will
         // create the document for me
         userDocRef.set({
-          name: this.state.signUpName
+          name: signUpName
         });
-        this.props.updateStatus();
+        props.updateStatus();
       }
     } catch (err) {
       console.log(err);
     }
   }
-  
+
   // Check out this link to learn more about firebase.auth()
   // https://firebase.google.com/docs/reference/node/firebase.auth.Auth
-  login = async () => {
+  const login = async () => {
     try {
       // Note that we don't have to tell the app that the user has logged in.
       // firebase.auth().onAuthStateChanged() in App.js communicates this for us!
-      await firebase.auth().signInWithEmailAndPassword(this.state.loginEmail, this.state.loginPassword)
+      await firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
     } catch (err) {
       console.log(err);
     }
   }
 
-  render() {
-    return (
+  return (
       <SafeAreaView style={styles.container}>
 
         <TextInput
           style={styles.input}
-          value={this.state.signUpName}
-          onChangeText={(signUpName) => this.setState({ signUpName })}
-          placeholder="Name" 
+          value={signUpName}
+          onChangeText={signUpName => setSignUpName(signUpName)}
+          placeholder="Name"
         />
         <TextInput
           style={styles.input}
-          value={this.state.signUpEmail}
-          onChangeText={(signUpEmail) => this.setState({ signUpEmail })}
-          placeholder="Email" 
+          value={signUpEmail}
+          onChangeText={signUpEmail => setSignUpEmail(signUpEmail)}
+          placeholder="Email"
         />
         <TextInput
           style={styles.input}
-          value={this.state.signUpPassword}
+          value={signUpPassword}
           secureTextEntry={true}
-          onChangeText={(signUpPassword) => this.setState({ signUpPassword })}
-          placeholder="Password" 
+          onChangeText={signUpPassword => setSignUpPassword(signUpPassword)}
+          placeholder="Password"
         />
         <Button
           title="Sign Up"
-          onPress={()=> this.signUp()}
+          onPress={signUp}
           color='#ffa000'
         />
 
 
         <TextInput
           style={[styles.input, {marginTop: 50}]}
-          value={this.state.loginEmail}
-          onChangeText={(loginEmail) => this.setState({ loginEmail })}
-          placeholder="Email" 
+          value={loginEmail}
+          onChangeText={loginEmail => setLoginEmail(loginEmail)}
+          placeholder="Email"
         />
         <TextInput
           style={styles.input}
-          value={this.state.loginPassword}
+          value={loginPassword}
           secureTextEntry={true}
-          onChangeText={(loginPassword) => this.setState({ loginPassword })}
-          placeholder="Password" 
+          onChangeText={loginPassword => setLoginPassword(loginPassword)}
+          placeholder="Password"
         />
         <Button
           title='Login'
-          onPress={()=> this.login()}
+          onPress={login}
           color='#ffa000'
         />
 
       </SafeAreaView>
-    );
-  }
+  );
 }
 
 const styles = StyleSheet.create({
